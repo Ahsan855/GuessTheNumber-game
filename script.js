1a8e3968754769979c69c7656dda9fa9a3a8ab89
Ahsan855 <a.r.ahsanhabib9869@gmail.com>
2023-09-02 10:41:11 +0600

    When guess is wrong this code is here

--- script.js
+++ script.js
@@ -18,22 +18,33 @@ const displayMessage = function (message) {
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-});
 
-//  When There is no input
+  //  When There is no input
 
-if (!guess) {
-  displayMessage('⛔️ No number!');
-}
-// when player wins
-else if (guess === secretNumber) {
-  displayMessage('🎉 Correct Number!');
-  document.querySelector('.number').textContent = secretNumber;
-  document.querySelector('body').style.backgroundColor = '#60b347';
-  document.querySelector('.number').style.width = '30rem';
-
-  if (score > highscore) {
-    highscore = score;
-    document.querySelector('.highscore').textContent = highscore;
+  if (!guess) {
+    displayMessage('⛔️ No number!');
   }
-}
+  // when player wins
+  else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+  // When guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
